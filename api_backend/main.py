fda05a8c3dde9075ef5f445a68c802130ec97747
vishalvinayT <vishalvinayt@gmail.com>
2023-05-06 18:35:30 +0200

    :construction: get and post are done need to test post

--- api_backend/main.py
+++ api_backend/main.py
@@ -1,11 +1,15 @@
-from fastapi import FastAPI
-from schemas import Warehouse
+import random
+from fastapi import FastAPI, HTTPException
+from schemas import Warehouse, Products, Companies, ProductsInfo
 import uvicorn
-from sqlalchemy import Table,Column, create_engine, MetaData
+import re
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import URL
 import os
 
+MAX_SHORT=65535
+PRIME=37
 DRIVER= "mysql"
 USERNAME= os.getenv("MySqlUserName", None)
 PASSWORD= os.getenv("MySqlPassword", None)
@@ -33,25 +37,78 @@ conn=engine.connect()
 # get warehouses
 @app.get("/getWarehouses")
 def getWareHouses():
-    #warehouses=Table('warehouses',metadata,autoload_with=engine)
     result=session.query(Warehouse).all()
     return result
     
 # get products
 @app.get("/getProducts")
 def getProducts():
-    products=Table('products',metadata,autoload_with=engine)
-    result=session.query(products)
-    data=[dict(row) for row in result]
-    return {"data":data}
+    result=session.query(ProductsInfo).all()
+    return result
 
 # get companies
 @app.get("/getCompanies")
 def getCompanies():
-    companies=Table('companies',metadata,autoload_with=engine)
-    result=session.query(companies)
+    result=session.query(Companies).all()
     return result
+
+# post warehouse
+@app.post("/addWarehouse")
+def addWarehouse(warehouse: Warehouse):
+    id=generateUniqueId("Warehouse"+warehouse.warehouseName)
+    if(stringChecker(warehouse.warehouseName , warehouse.city , warehouse.country)):
+        newWarehouse=Warehouse(id,warehouse.warehouseName, warehouse.city, warehouse.country)
+        session.add(newWarehouse)
+        session.commit()
+        return
+    raise HTTPException(status_code=400, detail="Please enter valid Informaton")
+
+
+@app.post("/addCompany")
+def addCompany(company: Companies):
+    id=generateUniqueId("Company"+company.companyName)
+    if(stringChecker(company.companyName, company.city,company.country, company.pincode,company.street)):
+        result=session.query(Warehouse).filter_by(Warehouse.id==company.warehouseId)
+        if(result is not None):
+            newCompany=Companies(id,company.warehouseId,company.companyName,company.street,company.city,company.pincode,company.country)
+            session.add(newCompany)
+            session.commit()
+            return
+    raise HTTPException(status_code=400, detail="Please enter valid Informaton")
+
+@app.post("/addPorduct")
+def addProduct(product:Products):
+    id=generateUniqueId("Product"+product.productName)
+    if(stringChecker(product.productName) and product.price>0 and product.quantity>0):
+        result=session.query(Companies).filter_by(Companies.id==product.companyId)
+        if(result is not None):
+            image=processImage(product.productImage)
+            newCompany=Products(id,product.companyId,product.productName,product.description,product.mfdDate,product.expDate,product.quantity,product.price,image)
+            session.add(newCompany)
+            session.commit()
+            return
+    raise HTTPException(status_code=400, detail="Please enter valid Informaton")
+
+
+def generateUniqueId(value: str)->int:
+    value= re.sub("[-_ *&()@.,/]","",value)
+    result=hash(value)*PRIME + random.randint(MAX_SHORT)
+    result=result *-1 if result<0 else result
+    return result
+
+def processImage(image: str)-> bytearray:
+    with open(image, "rb") as fp:
+        return fp.read()
+
+def stringChecker(*args)->bool:
+    for value in args:
+        value=re.sub("\s+","",value)
+        if(not(value)):
+            return False
+    return True
+
 
 if __name__=="__main__":
     uvicorn.run(app,host="localhost",port=8089)
-    #getWareHouses()
+    #getProducts()
+  
--- api_backend/schemas.py
+++ api_backend/schemas.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, INTEGER, VARCHAR, PrimaryKeyConstraint
+from sqlalchemy import Column, INTEGER, VARCHAR, DATETIME, DOUBLE, LargeBinary
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
 
 base=declarative_base()
@@ -11,4 +10,37 @@ class Warehouse(base):
     city=Column("city",VARCHAR)
     country=Column("country",VARCHAR)
 
-    
+
+class Companies(base):
+    __tablename__="companies"
+    id=Column("id",INTEGER, primary_key=True)
+    warehouseId=Column("warehouse_id", INTEGER)
+    companyName=Column("companyName",VARCHAR)
+    street= Column("street", VARCHAR)
+    city=Column("city",VARCHAR)
+    pincode= Column("pincode", VARCHAR)
+    country=Column("country",VARCHAR)
+ 
+class ProductsInfo(base):
+    __tablename__="products"
+    id=Column("id",INTEGER, primary_key=True)
+    companyId=Column("company_id",INTEGER)
+    productName=Column("productName",VARCHAR)
+    description=Column("description", VARCHAR)
+    mfdDate=Column("mfd_date", DATETIME)
+    expDate=Column("exp_date",DATETIME)
+    quantity=Column("quantity", INTEGER)
+    price=Column("price",DOUBLE)
+
+
+class Products(base):
+    __tablename__="products"
+    id=Column("id",INTEGER, primary_key=True)
+    companyId=Column("company_id",INTEGER)
+    productName=Column("productName",VARCHAR)
+    description=Column("description", VARCHAR)
+    mfdDate=Column("mfd_date", DATETIME)
+    expDate=Column("exp_date",DATETIME)
+    quantity=Column("quantity", INTEGER)
+    price=Column("price",DOUBLE)
+    productImage=Column("productImage",LargeBinary)
