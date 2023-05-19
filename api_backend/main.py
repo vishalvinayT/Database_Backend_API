531cfa0d269b8fdb2ff74e9d11d78e25d260c625
vishalvinayT <vishalvinayt@gmail.com>
2023-05-19 22:34:56 +0200

    :construction: on filtering fixes

--- api_backend/main.py
+++ api_backend/main.py
@@ -1,9 +1,11 @@
 import random
+from fastapi.responses import ORJSONResponse
 from fastapi import FastAPI, HTTPException, UploadFile, File, Depends
 from schemas import Warehouse, Companies, ProductsInfo, Products
 import uvicorn
 from datetime import datetime
 from typing import Optional
+from copy import deepcopy, copy
 import re
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
@@ -89,41 +91,50 @@ def getCompanies():
 # post warehouse
 @app.post("/addWarehouse", response_model=WarehouseCreate)
 def addWarehouse(warehouse: WarehouseCreate):
-    id=generateUniqueId("Warehouse"+warehouse.warehouseName)
-    if(stringChecker(warehouse.warehouseName , warehouse.city , warehouse.country)):
-        newWarehouse=Warehouse(id=id,warehouseName=warehouse.warehouseName, city=warehouse.city,country=warehouse.country)
-        session.add(newWarehouse)
-        session.commit()
-        return warehouse
-    raise HTTPException(status_code=400, detail="Please enter valid Informaton")
+    try:
+        id=generateUniqueId("Warehouse"+warehouse.warehouseName)
+        if(stringChecker(warehouse.warehouseName , warehouse.city , warehouse.country)):
+            newWarehouse=Warehouse(id=id,warehouseName=warehouse.warehouseName, city=warehouse.city,country=warehouse.country)
+            session.add(newWarehouse)
+            session.commit()
+            return ORJSONResponse({"message":"Added data"}, status_code=201)
+        return ORJSONResponse({"message":"Invalid informatation"}, status_code=400)
+    except Exception:
+        return ORJSONResponse({"message":"Something Went Wrong"}, status_code=404)
 
 
 @app.post("/addCompany")
 def addCompany(company: CompaniesCreate):
-    id=generateUniqueId("Company"+company.companyName)
-    if(stringChecker(company.companyName, company.city,company.country, company.pincode,company.street)):
-        result=session.query(Warehouse).filter(Warehouse.id==company.warehouseId)
-        if(result is not None):
-            newCompany=Companies(id=id,warehouseId=333278322,companyName="vishal",street="kaiser",city="aachen",country="Germany")
-            session.add(newCompany)
-            session.commit()
-            return company
-    raise HTTPException(status_code=400, detail="Please enter valid Informaton")
+    try:
+        id=generateUniqueId("Company"+company.companyName)
+        if(stringChecker(company.companyName, company.city,company.country, company.pincode,company.street)):
+            result=session.query(Warehouse).filter(Warehouse.id==company.warehouseId)
+            if(bool(result.one())):
+                newCompany=Companies(id=id,warehouseId=333278322,companyName="vishal",street="kaiser",city="aachen",country="Germany")
+                session.add(newCompany)
+                session.commit()
+                return ORJSONResponse({"message":"Added data"}, status_code=201)
+        return ORJSONResponse({"message":"Invalid informatation"}, status_code=400)
+    except Exception:
+        return ORJSONResponse({"message":"Something went wrong"}, status_code=404)
 
 @app.post("/addProduct")
 async def addProduct(product:ProductsCreate = Depends(), insertImage: UploadFile = File(...)):
-    id=generateUniqueId("Product"+product.productName)
-    if(stringChecker(product.productName) and product.price>0 and product.quantity>0):
-        result=session.query(Companies).filter(Companies.id==product.companyId)
-        if(result is not None):
-            image=await insertImage.read()
-            print(type(image))
-            newProduct=Products(id=id,companyId=product.companyId,productName=product.productName,description=product.description,mfdDate=formatDate(product.mfdDate),expDate=formatDate(product.expDate),quantity=product.quantity,price=product.price,productImage=image)
-            session.add(newProduct)
-            session.commit()
-            return product
-    raise HTTPException(status_code=400, detail="Please enter valid Informaton")
-
+    try:
+        id=generateUniqueId("Product"+product.productName)
+        if(stringChecker(product.productName) and product.price>0 and product.quantity>0):
+            result=session.query(Companies).filter(Companies.id==product.companyId)
+            if(bool(result.one())):
+                image=await insertImage.read()
+                print(type(image))
+                newProduct=Products(id=id,companyId=product.companyId,productName=product.productName,description=product.description,mfdDate=formatDate(product.mfdDate),expDate=formatDate(product.expDate),quantity=product.quantity,price=product.price,productImage=image)
+                session.add(newProduct)
+                session.commit()
+                return ORJSONResponse({"message":"Added data"}, status_code=201)
+        return ORJSONResponse({"message":"Invalid informatation"}, status_code=400)
+    except Exception:
+        return ORJSONResponse({"message":"Something went wrong"}, status_code=404)
+    
 @app.put("/putProduct/{productName}")
 async def putProdutInfo(productName: str,description: str, image: UploadFile= File(...)):
     image=await image.read()
@@ -138,6 +149,41 @@ def putCompanyInfo(companyName: str, company: CompaniesCreate):
     return {"message":"ok"}
 
 
+@app.get("/searchByCompanies")
+def findCompany(companyName: str):
+    res=session.query(Companies).filter(Companies.companyName==companyName)
+    if(bool(res.all())):
+        return res
+    return ORJSONResponse({"message":"Bad Request"}, status_code=400)
+
+
+@app.get("/searchByProducts")
+def findProduct(productName: str):
+    res=session.query(Products).filter(Products.productName==productName)
+    if(bool(res.all())):
+        return res
+    return ORJSONResponse({"message":"Bad Request"}, status_code=400)
+
+@app.delete("/deleteCompany/{companyName}")
+def deleteCompany(companyName: str):
+    session.query(Companies).filter(Companies.companyName==companyName).delete(synchronize_session=False)
+    session.commit()
+    return {"message":"ok"}
+
+@app.delete("/deleteProduct/{productName}")
+def deleteCompany(productName: str):
+    session.query(Products).filter(Products.productName==productName).delete(synchronize_session=False)
+    session.commit()
+    return {"message":"ok"}
+
+@app.delete("/deleteWarehouse/{warehouseName}")
+def deleteCompany(warehouseName: str):
+    session.query(Warehouse).filter(Warehouse.warehouseName==warehouseName).delete(synchronize_session=False)
+    session.commit()
+    return {"message":"ok"}
+
+
+## Validators ##
 def generateUniqueId(value: str)->int:
     value= re.sub("[-_ *&()@.,/]","",value)
     result=(abs(hash(value)) % (10 ** 6))*PRIME + random.randint(0,MAX_SHORT)
@@ -157,14 +203,13 @@ def stringChecker(*args)->bool:
 
 
 def queryDebug():
-    id=generateUniqueId("Company"+"vishal")
-    if(stringChecker("vishal","aachen","kasier","aachen","Germany")):
-        result=session.query(Warehouse).filter(Warehouse.id==333278322)
-        if(result is not None):
-            newCompany=Companies(id=id,warehouseId=333278322,companyName="vishal",street="kaiser",city="aachen",country="Germany")
-            session.add(newCompany)
-            #session.commit()
-            return True
+    from copy import copy
+    ## test function to debug ##
+    res=session.query(Companies).filter(Companies.companyName=="ahdkjdbh")
+    copyRes=copy(res)
+    if(bool(copyRes.one())):
+        print("result: ",bool(res))
+        return copyRes.all()
 
 
 if __name__=="__main__":
